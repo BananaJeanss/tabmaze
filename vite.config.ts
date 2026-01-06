cbd3535a47053091f03d5cdcdc3d003b76887fcc
BananaJeans <18456310+BananaJeanss@users.noreply.github.com>
2026-01-06 21:50:23 +0200

    proper pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,53 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    VitePWA({ registerType: "autoUpdate", devOptions: { enabled: true } }),
+    mkcert(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      manifest: {
+        name: "tabmaze",
+        short_name: "tabmaze",
+        description: "escape the maze, using tab, and some other keys",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+        screenshots: [
+          {
+            src: "/screenshot.png",
+            sizes: "960x875",
+            type: "image/png",
+            form_factor: "wide",
+          },
+          {
+            src: "/screenshot.png",
+            sizes: "960x875",
+            type: "image/png",
+            form_factor: "narrow",
+          },
+        ],
+      },
+    }),
   ],
 });
